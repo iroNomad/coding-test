8ed8eeb66133d26ad7146d7c4f2c9528e87e1d30
Jaka Buteelch <89756508+iroNomad@users.noreply.github.com>
2025-04-22 23:18:56 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 8.00 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] solution(String s) {
+        
+        String mainStr = s;
+        int strLen = 0;
+        
+        int zeroCount = 0;
+        int rotCount = 0;
+        
+        while (!mainStr.equals("1")) {
+            for (char c : mainStr.toCharArray()) {
+                if (c == '0') {
+                    zeroCount++;
+                }
+                else {
+                    strLen++;
+                }
+            }            
+            mainStr = Integer.toBinaryString(strLen);
+            strLen = 0;
+            rotCount++;
+        }
+        
+        int[] answer = {rotCount, zeroCount};
+        
+        return answer;
+    }
+}
