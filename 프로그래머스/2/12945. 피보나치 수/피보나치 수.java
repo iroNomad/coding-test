fdd0f5376303f3bddf4483692b28ba258faa1d9e
Jaka Buteelch <89756508+iroNomad@users.noreply.github.com>
2025-04-23 22:24:54 +0900

    [level 2] Title: 피보나치 수, Time: 359.07 ms, Memory: 575 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -6,15 +6,13 @@ class Solution {
         return fib(n);
     }
     public int fib(int num) {
-        
-        int result = 0;
-        if (memo.containsKey(num)) {
-            return memo.get(num);
-        }
         if (num == 0 || num == 1) {
             return num;
         }
-        result = (fib(num - 1) + fib(num - 2)) % 1234567;
+        if (memo.containsKey(num)) {
+            return memo.get(num);
+        }
+        int result = (fib(num - 1) + fib(num - 2)) % 1234567;
         memo.put(num, result);
         return result;
     }
