9d76f740c66f8a267d888c5449c53faa6acad333
Jaka Buteelch <89756508+iroNomad@users.noreply.github.com>
2025-06-17 23:51:11 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -0,0 +1,12 @@
+function solution(num_list) {
+    var answer = 0;
+    if (num_list.length > 10) {
+        num_list.forEach(e => answer += e)
+    }
+    else {
+        answer = 1;
+        num_list.forEach(e => answer *= e)
+    }
+    
+    return answer;
+}
